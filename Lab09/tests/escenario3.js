de1f0a58395a2fcf41277431791e5ff35874c5c5
Gabriela Borda <enayely142@gmail.com>
2026-07-16 09:31:08 -0500

    Ejercicio 3. Pruebas de rendimiento utilizando K6

---
+++ Lab09/tests/escenario3.js
@@ -0,0 +1,37 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    vus: 100,
+    duration: '60s',
+};
+
+export default function () {
+
+    const payload = JSON.stringify({
+        concierto_id: 1,
+        comprador: "Usuario K6",
+        correo: "usuario@test.com",
+        cantidad: 1,
+        metodo_pago: "Tarjeta"
+    });
+
+    const params = {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+
+    const res = http.post(
+        "http://127.0.0.1:5000/ventas",
+        payload,
+        params
+    );
+
+    check(res, {
+        "status 201": (r) => r.status === 201,
+        "respuesta menor a 500 ms": (r) => r.timings.duration < 500,
+    });
+
+    sleep(1);
+}
